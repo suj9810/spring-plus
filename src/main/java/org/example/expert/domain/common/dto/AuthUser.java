cd44c24bf6fd4f448b94fc76a3a6dfee2fec3574
suj9810 <suj9810@naver.com>
2025-05-11 20:14:38 +0900

    레벨1 - JWT의 이해

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -8,11 +8,13 @@ public class AuthUser {
 
     private final Long id;
     private final String email;
+    private final String nickname;
     private final UserRole userRole;
 
     public AuthUser(Long id, String email, UserRole userRole) {
         this.id = id;
         this.email = email;
+        this.nickname = email;
         this.userRole = userRole;
     }
 }
