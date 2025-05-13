1f34e49720b24e4ad8e3a8cffc73faf79bce9c8e
suj9810 <suj9810@naver.com>
2025-05-13 09:40:12 +0900

    레벨1-3 JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,4 +19,15 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query("SELECT t FROM Todo t "
+        + "WHERE (:weather IS NULL OR t.weather = :weather)"
+        + "AND (:modifiedStart IS NULL OR t.modifiedAt >= :modifiedStart)"
+        + "AND (:modifiedEnd IS NULL OR t.modifiedAt <= :modifiedEnd)")
+    Page<Todo> searchTodos(
+        @Param("weather") String weather,
+        @Param("modifiedStart")LocalDateTime modifiedStart,
+        @Param("modifiedEnd") LocalDateTime modifiedEnd,
+        Pageable pageable
+    );
 }
